cfeef0ad3bd325ac2df2a9478a63c7f80f72ac91
Jonathan Alfonso <alfonsojon1997@gmail.com>
2015-04-12 02:03:45 -0500

    Use Chrome App Builder as a base

--- README.md
+++
@@ -1,2 +0,0 @@
-# anime-twist-crx
-An unofficial Chrome app to view Anime Twist on the desktop.
---
+++ config.js
@@ -0,0 +1,10 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Anime Twist",
+   "version": "0.2",
+   "homepage": "https://twist.moe/",
+   "enableReloadBttn": true,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
